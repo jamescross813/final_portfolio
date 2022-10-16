97f3c04b0ecc9c36ceec1d00450ae963e6806797
jamescross813 <crossjames813@gmail.com>
2022-10-16 14:47:39 -0500

    update about page layout

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -3,10 +3,10 @@ import React, { Component } from 'react'
 class AboutComponent extends Component{
     render(){
         return(
-            <div classname = "about-me">
-                <h1 className="about-me-title">About</h1>
-                <p className="about-me-line-one">Hi! My name is James Cross. I'm a Geek, a Nerd and a Coder.</p>
-                <p className="about-me-line-two">I've worn many hats over my career span and am excited for the challenges that come with my new path.</p>
+            <div classname = "container">
+                <h1 className="title">About</h1>
+                <p className="row">Hi! My name is James Cross. I'm a Geek, a Nerd and a Coder.</p>
+                <p className="row">I've worn many hats over my career span and am excited for the challenges that come with my new path.</p>
             </div>
         )
     }
