fc9024c4c9cac550404d1e1863ea01b865980e68
jamescross813 <crossjames813@gmail.com>
2022-10-17 20:32:04 -0500

    added to base component

--- src/components/BaseComponent.js
+++ src/components/BaseComponent.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import HeaderComponent from "./HeaderComponent"
+import NavBarComponent from "./NavBarComponent"
 
 class BaseComponent extends Component{
     render(){
         return(
-            <HeaderComponent/> 
+            <><NavBarComponent />
+            <HeaderComponent /></> 
         )
     }
 } 
