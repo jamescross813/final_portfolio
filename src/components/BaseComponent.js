0553336264caab1f2deab51bba28bd5abc5bbdaa
jamescross813 <crossjames813@gmail.com>
2022-10-17 20:29:19 -0500

    basecompoennt block

---
+++ src/components/BaseComponent.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import HeaderComponent from "./HeaderComponent"
+
+class BaseComponent extends Component{
+    render(){
+        return(
+            <HeaderComponent/> 
+        )
+    }
+} 
+
+export default BaseComponent
