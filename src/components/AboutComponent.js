87366e1900e3965619518435dece58b6f39101f0
jamescross813 <crossjames813@gmail.com>
2022-10-18 20:19:57 -0500

    update text color

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -6,7 +6,7 @@ class AboutComponent extends Component{
         return(
             <>
                 <BaseComponent/>
-                <div className="column w-25">
+                <div className="column w-25 text-light">
                             <h1 className="title">About</h1>
                             <p className="row">Hi! My name is James Cross. I'm a Geek, a Nerd and a Coder.</p>
                             <p className="row">I've worn many hats over my career span and am excited for the challenges that come with my new path.</p>
