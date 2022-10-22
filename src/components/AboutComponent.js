00f1d25fa87fc224ff00e189d7fd3bff4d09ab03
jamescross813 <crossjames813@gmail.com>
2022-10-21 21:15:06 -0500

    additional text

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -13,6 +13,7 @@ class AboutComponent extends Component{
                             <h1 className="title">About</h1>
                                 <p className="row">Hi! My name is James Cross. I'm a Geek, a Nerd and a Coder.</p>
                                 <p className="row">I've worn many hats over my career span and am excited for the challenges that come with my new path.</p>
+                                <p> I got into coding as a hobby, then delved deeper trhough Flatiron. Since graduating I've continued my studes, expanded my knowledge base and gotten to use the skills I've learnt in the real world!</p>
                 </div>
             </div>
             </>
